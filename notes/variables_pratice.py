bfc2bd777b2e5cad5339e726b4a42b5b7e5c77cd
GAC17 <gage.christensen@ucas-edu.net>
2026-01-22 11:30:39 -0700

    I added what was needed variables practice

--- notes/variables_pratice.py
+++ notes/variables_pratice.py
@@ -9,4 +9,14 @@ number2 = 250
 breakfast = "eggs and toast"
 
 color = ("Blue")
-print(name, number, number2, breakfast, color)
+
+school = ("UCAS")
+
+year = ("2026")
+
+eye = ("hazel")
+
+age = 15
+
+subject = ("Health science")
+print(name, number, number2, breakfast, color, school, year, eye, age, subject)
