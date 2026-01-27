e2d5ecd464b6a67118ca39bc932caf310b51dde9
GAC17 <gage.christensen@ucas-edu.net>
2026-01-27 11:50:14 -0700

    notes

--- notes/finanical_calc.py
+++ notes/finanical_calc.py
@@ -6,11 +6,11 @@ util = float (input("What is your utility bill?"))
 grocery = float (input("What is your grocery bill?"))
 transport = float (input("What is your transportation bill?"))
 
-total = round (rent/income) * 100 2
-total1 =round (util/income) *100 2
-total2 = round (grocery/income)*100 2
-total3 = round (transport/income) *100 2
-print("Your rent is", (total), "of your income")
-print("Your util is", (total1), "of your income")
-print("Your grocery bill is", (total2), "of your income")
-print("Your transportation bill is", (total3), "of your income")
+total = round ((rent/income) * 100, 2)
+total1 =round ((util/income) *100, 2)
+total2 = round ((grocery/income)*100, 2)
+total3 = round ((transport/income) *100, 2)
+print("Your rent is", income, "which is %", total, "of your income")
+print"Your rent is", income, "which is %", total, "of your income")
+print("Your grocery bill is", total2, "of your income")
+print("Your transportation bill is", total3, "of your income")
---
+++ notes/strings.py
@@ -0,0 +1,19 @@
+"""color = input("What is your favorite color?").strip().title()
+
+print(color, "is a really cool color")"""""""""
+
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+sentence = "The quick brown fox jumps over the lazy dog"
+
+"""print(alphabet[4:8])
+word= input("What word do you want to change: ")
+new = input("What will the new word be").
+start = sentence.find(word)
+print(sentence[start:start+len(word)])
+print(sentence)
+sentence = sentence.replace(word, new)
+print(sentence)"""
+
+number = int (input("tell me a number:"))
+print(number/5)
+
