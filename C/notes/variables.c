2c67077732f71dfaaec5968792e5c38fa011f1c2
GAC17 <gage.christensen@ucas-edu.net>
2026-02-18 11:48:48 -0700

    notesday

---
+++ C/notes/variables.c
@@ -0,0 +1,10 @@
+// This is a comment 
+#include <stdio.h> // lets us create inputs and outputs
+int main(){
+    int number = 12;
+    float pi = 3.14;
+    printf("%d\n", number); // \n tells the program to make a new line
+    printf("%f\n", pi);  
+    return 0;
+
+}
