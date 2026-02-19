5a3752a84981769f13bc21dab237b589fae6773c
GAC17 <gage.christensen@ucas-edu.net>
2026-02-19 11:37:04 -0700

    varic

--- C/notes/variables.c
+++ C/notes/variables.c
@@ -3,8 +3,19 @@
 int main(){
     int number = 12;
     float pi = 3.14;
-    printf("%d\n", number); // \n tells the program to make a new line
-    printf("%f\n", pi);  
-    return 0;
+    char name [] = "Sawyer";
+    char person[50]; 
+    int age;
+    printf("tell me your name\n");
+    scanf("%s", &person);
+    printf("Name is %s and age is %d\n", person, age);
 
+    printf("Tell me how old you are\n");
+    scanf("%d", &age);
+
+
+    printf("%d\n", number); // \n tells the program to make a new line
+    printf("%f\n", pi);
+    printf("%s is %d years old\n", name , age);  
+    return 0;
 }
---
+++ C/notes/variprac.c
@@ -0,0 +1,17 @@
+// GC LaRose 3RD period
+#include <stdio.h>
+int main(){
+    char name[] = "Gage";
+    int lucky = 7;
+    float deci = 3.14;
+    char breakf[] = "Eggs and hot sauce";
+    char color[] = "Green";
+    char skl[]= "UCAS";
+    int yr = 2026;
+    char eyec[] = "Hazel";
+    int age = 15;
+    char subj[] = "Health Science";
+
+    printf("Name: %s,\n Lucky number: %d\n Decimal number: %f,\n Breakfast: %s,\n Favorite Color: %s,\n School name: %s,\n Year: %d, \n Eye color: %s, \n Age: %d,  \n Favorite subject: %s\n", name, lucky, deci, breakf, color, skl, yr, eyec, age, subj);
+    return 0;
+}
