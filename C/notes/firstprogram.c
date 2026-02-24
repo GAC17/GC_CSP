f987b991ecae46ddde262d2de7819e3ebec85f67
GAC17 <gage.christensen@ucas-edu.net>
2026-02-24 08:42:52 -0700

    firstprogra

--- C/notes/firstprogram.c
+++ C/notes/firstprogram.c
@@ -1,1 +1,9 @@
-#include <stdio.>
+#include <stdio.h>
+// GC 3rd period LaRose
+int main (){
+    char name [50];
+    printf("What is your name? ");
+    scanf("%s", name);
+    printf("\nHello, %s.", name);
+    return 0;
+}
