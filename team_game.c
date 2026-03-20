b001f9798a4857de6333d75a8499412a15ab8a49
GAC17 <gage.christensen@ucas-edu.net>
2026-03-20 11:49:15 -0600

    teamgroup

--- team_game.c
+++ team_game.c
@@ -15,11 +15,10 @@ int main (){
         }else{
             printf("You still must play\n");
         };
-    
-   
 
     while (true){
-          char spts[9] = {'0','1','2','3','4','5','6','7','8'};
+          
+        char spts[9] = {'0','1','2','3','4','5','6','7','8'};
 
         printf(" %c | %c | %c \n", spts[0], spts[1], spts[2]);
         printf(" %c | %c | %c \n", spts[3], spts[4], spts[5]);
@@ -27,8 +26,8 @@ int main (){
    
    
    
-        int myNum;
-      printf("Select a board spot 0-8:\n");
+    int myNum;
+        printf("Select a board spot 0-8:\n");
      scanf("%d", &myNum);
 
      if(myNum == 0){
@@ -65,13 +64,23 @@ int main (){
      }
  if(myNum == 8){
     spts[8] = 'X';
+ }
 
 
 
-    srand(time(NULL));
+printf ("\n");
+ printf("Players' Turn\n");
+        printf(" %c | %c | %c \n", spts[0], spts[1], spts[2]);
+        printf(" %c | %c | %c \n", spts[3], spts[4], spts[5]);
+        printf(" %c | %c | %c \n", spts[6], spts[7], spts[8]);
+
+
+
+srand(time(NULL));
  
 int bot= rand() %9; //bot turn here
   if(bot == 0){
+    
     spts[0] = 'O';
         
      }
@@ -101,50 +110,55 @@ int bot= rand() %9; //bot turn here
      }
  if(bot == 7){
     spts[7] = 'O';
-        
-     }
+ } 
+printf("Bots turn\n");
+   
+
  if(bot == 8){
     spts[8] = 'O';
     
-        printf(" %c | %c | %c \n", spts[0], spts[1], spts[2]);
-        printf(" %c | %c | %c \n", spts[3], spts[4], spts[5]);
-        printf(" %c | %c | %c \n", spts[6], spts[7], spts[8]);
-}
-        printf(" %c | %c | %c \n", spts[0], spts[1], spts[2]);
-        printf(" %c | %c | %c \n", spts[3], spts[4], spts[5]);
-        printf(" %c | %c | %c \n", spts[6], spts[7], spts[8]);
+
+        
+     }   
+     
+    
+   
+    }
+    
 
           /*char* win = spts;
-    if (spts[0]==spts[1]==spts[2]){
+    if (spts[0]==spts[1] && spts[1] == spts[2]){
         printf("You win!");
         break;
-    }eif(spts[3]==spts[4]==spts[5]){
+    }else if(spts[3]==spts[4] && spts[5] == spts[4]){
         printf("You Win!");
         break;
-    } else if(spts[6]==spts[7]==spts[8]){
+    } else if(spts[6]==spts[7]&& spts[8]==spts[7]){
         printf("You Win!");
         break;
-    }else if(spts[0]==spts[4]==spts[8]){
+    }else if(spts[0]==spts[4]&&spts[8]==spts[4]){
         printf("You Win!");
         break;
-    }else if(spts[6]==spts[4]==spts[2]){
+    }else if(spts[6]==spts[4]&& spts[2]==spts[4]){
         printf("You Win!");
         break;
-    }else if(spts[6]==spts[7]==spts[8]){
+    }else if(spts[6]==spts[7]&&spts[8] == spts[7]){
         printf("You win!");
         break;
-    }else if( spts[0]==spts[3]==spts[6]){
+    }else if( spts[0]==spts[3]&&spts[6] == spts[3]){
         printf("You win!");
         break;
-    }else if(spts[1]==spts[4]==spts[7]){
+    }else if(spts[1]==spts[4]&& spts[7] == spts[4]){
         printf("You win!");
         break;
-    }else if(spts[2]==spts[5]==spts[8]){
+    }else if(spts[2]==spts[5]&& spts[8] ==spts[5]){
         printf("You win!");
         break;
-        */
-    }
-       
+    } 
+
+    
+}*/
+
+return 0;
 }
-    return 0;
-  }
+
